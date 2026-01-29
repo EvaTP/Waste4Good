63e4c89f6ae06cefefe4630144a98fee57263634
EvaTP <evathpr@icloud.com>
2026-01-29 12:13:42 +0100

    fix(dashboard route): waste value instead of waste name and id instead of firstname

--- waste4good-back/routes/dashboard.js
+++ waste4good-back/routes/dashboard.js
@@ -1,29 +1,28 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../db');
+const pool = require("../db");
 
-
-router.get('/:firstname', async (req, res) => {
-  const { firstname } = req.params;
+router.get("/:userId", async (req, res) => {
+  const { userId } = req.params;
   try {
     const result = await pool.query(
-      `SELECT w.name AS type, SUM(ic.quantity) AS total_quantity
+      `SELECT w.value AS type, SUM(ic.quantity) AS total_quantity
        FROM is_collected ic
        JOIN collections c ON ic.collection_id = c.id
        JOIN volunteers v ON c.volunteer_id = v.id
        JOIN wastes w ON ic.waste_id = w.id
-       WHERE v.firstname = $1
-       GROUP BY v.id, w.name`,
-      [firstname]
+       WHERE v.id = $1
+       GROUP BY v.id, w.value`,
+      [userId],
     );
 
     res.json(result.rows);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Erreur lors de la récupération des collectes' });
+    res
+      .status(500)
+      .json({ error: "Erreur lors de la récupération des collectes" });
   }
 });
-
 
 module.exports = router;
