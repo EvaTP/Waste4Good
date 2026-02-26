93f710bbcd44c942e693670f2ca914beab54f12c
EvaTP <evathpr@icloud.com>
2026-02-26 15:02:33 +0100

    fix(dashboard route):date

--- waste4good-back/routes/dashboard.js
+++ waste4good-back/routes/dashboard.js
@@ -4,7 +4,9 @@ const pool = require("../db");
 
 router.get("/:userId", async (req, res) => {
   const { userId } = req.params;
-  const { month, year } = req.query; // Récupérer les paramètres de mois et d'année
+  const month = parseInt(req.query.month); // Récupérer le mois depuis les paramètres de requête
+  const year = parseInt(req.query.year); // Récupérer l'année depuis les paramètres de requête
+  // const { month, year } = req.query; // Récupérer les paramètres de mois et d'année
   try {
     const result = await pool.query(
       `SELECT w.value AS type, SUM(ic.quantity) AS total_quantity
@@ -13,8 +15,8 @@ router.get("/:userId", async (req, res) => {
        JOIN volunteers v ON c.volunteer_id = v.id
        JOIN wastes w ON ic.waste_id = w.id
        WHERE v.id = $1
-        AND EXTRACT(MONTH FROM ic.created_at) = $2
-        AND EXTRACT(YEAR FROM ic.created_at) = $3
+        AND EXTRACT(MONTH FROM c.created_at) = $2
+        AND EXTRACT(YEAR FROM c.created_at) = $3
        GROUP BY v.id, w.value`,
       [userId, month, year],
     );
