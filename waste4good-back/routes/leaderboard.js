f7c2d95a42766afef0440233c11a8a10df3c86d3
EvaTP <evathpr@icloud.com>
2026-03-01 19:09:32 +0100

    update(leadearboard route)with total collections

--- waste4good-back/routes/leaderboard.js
+++ waste4good-back/routes/leaderboard.js
@@ -1,24 +1,22 @@
-
-
 // Route avec toutes les associations
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../db');
+const pool = require("../db");
 
-router.get('/', async (req, res)=>{
-  try{
+router.get("/", async (req, res) => {
+  try {
     const results = await pool.query(`
-      SELECT v.firstname,
-      SUM(c.number_collections) AS total_collections
-      FROM collections c
+      SELECT v.firstname, SUM(ic.quantity) AS total_collections
+      FROM is_collected ic
+      JOIN collections c ON c.id = ic.collection_id
       JOIN volunteers v ON v.id = c.volunteer_id
       GROUP BY v.id, v.firstname
       ORDER BY total_collections DESC
     `);
     res.json(results.rows);
-  } catch(err) {
-    res.status(500).json({error: 'Erreur serveur' });
-  };
+  } catch (err) {
+    res.status(500).json({ error: "Erreur serveur" });
+  }
 });
 
 module.exports = router;
