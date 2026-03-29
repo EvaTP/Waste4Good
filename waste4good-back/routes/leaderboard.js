550e3f526f63c3fbbc747ad93bbfc1d9167832a2
EvaTP <evathpr@icloud.com>
2026-03-29 16:19:27 +0200

    update(Leaderboard route): add donations

--- waste4good-back/routes/leaderboard.js
+++ waste4good-back/routes/leaderboard.js
@@ -6,10 +6,19 @@ const pool = require("../db");
 router.get("/", async (req, res) => {
   try {
     const results = await pool.query(`
-      SELECT v.firstname, SUM(ic.quantity) AS total_collections
+      SELECT 
+        v.firstname,
+        SUM(ic.quantity) AS total_collections,
+        COALESCE(SUM(d.donated_points), 0) AS total_donated_points,
+        COALESCE(
+          STRING_AGG(DISTINCT a.name, ', '), 
+          'Aucun don'
+        ) AS associations_supported
       FROM is_collected ic
       JOIN collections c ON c.id = ic.collection_id
       JOIN volunteers v ON v.id = c.volunteer_id
+      LEFT JOIN donations d ON d.volunteer_id = v.id
+      LEFT JOIN associations a ON a.id = d.association_id
       GROUP BY v.id, v.firstname
       ORDER BY total_collections DESC
     `);
