7125bcd169b7acf96d02e9d3dc04fe5b93045756
EvaTP <evathpr@icloud.com>
2026-03-29 16:20:08 +0200

    update(Leaderboard): add donations

--- waste4good-front/src/app/leaderboard/components/itemLeaderboard.jsx
+++ waste4good-front/src/app/leaderboard/components/itemLeaderboard.jsx
@@ -1,17 +1,24 @@
 "use client";
-import Image from "next/image";
 import styles from "../page.module.css";
-import layoutStyles from "../../styles/layout.module.css";
-//import { useState, useEffect } from "react";
+// import layoutStyles from "../../styles/layout.module.css";
 
 export default function ItemLeaderboard({ volunteer }) {
   return (
     <div className={styles.volunteer_item}>
       <div className={styles.volunteer_info}>
         <h3 className={styles.volunteer_info_name}>{volunteer.firstname}</h3>
+
+        {/* Collectes */}
         <p className={styles.volunteer_info_p}>
           {volunteer.total_collections} collecte
           {volunteer.total_collections > 1 ? "s" : ""}
+        </p>
+        {/* Donations */}
+        <p className={styles.volunteer_info_p}>
+          💚 {volunteer.total_donated_points} pts donnés
+        </p>
+        <p className={styles.volunteer_info_p}>
+          🌿 {volunteer.associations_supported}
         </p>
       </div>
     </div>
